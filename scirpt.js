5e6ffdbad3a26eebe2058b63ec7cd272ea4d32b3
Kasyuk Mikhail <kasyukmikhail@gmail.com>
2023-09-01 00:46:47 +0300

    Update JS

--- scirpt.js
+++ scirpt.js
@@ -1,25 +1,63 @@
 const buttons = document.querySelectorAll('button')
-// const pluses = buttons.querySelector('.plus')
+const bg = document.querySelector('.shadow');
 
-function colorCheck(btn) {
-    if (btn.classList.contains('blue')) {
-        btn.classList.toggle('blue-active');
-    } else {
-        btn.classList.toggle('green-active');
-    }
-}
+document.addEventListener('click', (e) => {
+    e.preventDefault();
+    let target = e.target;
+    
+    // let btn = e.target.closest('btn');
+    var plus = e.target.firstChild;
+    var location = e.target.lastChild;
+    console.log(target.tagName)
+    buttonsArray = Array.from(buttons)
 
-buttons.forEach(button =>
-    button.addEventListener('click', (e) => {
-        e.preventDefault();
-        var plus = button.firstChild
-        var location = button.lastChild
-
-        colorCheck(button);
-
-        button.classList.toggle('btn-active');
+    if (target.tagName == 'BUTTON') {
+        // console.log(e.target)
+        if (e.target.classList.contains('blue')) {
+            e.target.classList.toggle('blue-active')
+        } else {
+            e.target.classList.toggle('green-active')
+        }
+        e.target.classList.toggle('btn-active');
         plus.classList.toggle('plus-active')
         plus.classList.toggle('plus-vertical')
         location.classList.toggle('location-active')
-    })
-);
+    }
+    
+    if (target.tagName == 'DIV') {
+        buttonsArray.forEach(button => {
+            button.classList.remove('btn-active')
+            button.firstChild.classList.remove('plus-active')
+            button.firstChild.classList.add('plus-vertical')
+            button.lastChild.classList.remove('location-active')
+            if (button.classList.contains('blue-active')) {
+                button.classList.remove('blue-active')
+            } else {
+                button.classList.remove('green-active')
+            }
+        })
+            
+    }
+  
+})
+// })
+
+// buttons.forEach(button =>
+//     button.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         const clickOut = e.composedPath().includes(button);
+
+//         let target = e.target;
+//         var plus = button.firstChild
+//         var location = button.lastChild
+
+//         console.log(target)
+//         colorCheck(button);
+
+//         button.classList.toggle('btn-active');
+//         plus.classList.toggle('plus-active')
+//         plus.classList.toggle('plus-vertical')
+//         location.classList.toggle('location-active')
+
+//     })
+// );
